e8482df794adbd154cf1e49c31117846714eb40e
DAY <rachid.da@kuikops.com>
2023-11-15 11:31:32 +0100

    les requêtes sql

---
+++ steup.sql
@@ -0,0 +1,63 @@
+USE ROLE ACCOUNTADMIN;
+
+CREATE OR REPLACE WAREHOUSE Rachid_L;
+CREATE OR REPLACE DATABASE Rachid_DB;
+CREATE OR REPLACE SCHEMA Rachid_SCHEMA;
+
+USE Rachid_DB.Rachid_SCHEMA;
+
+
+CREATE or REPLACE file format csvformat
+  skip_header = 1
+  type = 'CSV';
+
+CREATE or REPLACE stage campaign_data_stage
+  file_format = csvformat
+  url = 's3://sfquickstarts/ad-spend-roi-snowpark-python-scikit-learn-streamlit/campaign_spend/';
+
+CREATE or REPLACE TABLE CAMPAIGN_SPEND (
+  CAMPAIGN VARCHAR(60), 
+  CHANNEL VARCHAR(60),
+  DATE DATE,
+  TOTAL_CLICKS NUMBER(38,0),
+  TOTAL_COST NUMBER(38,0),
+  ADS_SERVED NUMBER(38,0)
+);
+
+COPY into CAMPAIGN_SPEND
+  from @campaign_data_stage;
+
+CREATE or REPLACE stage monthly_revenue_data_stage
+  file_format = csvformat
+  url = 's3://sfquickstarts/ad-spend-roi-snowpark-python-scikit-learn-streamlit/monthly_revenue/';
+
+CREATE or REPLACE TABLE MONTHLY_REVENUE (
+  YEAR NUMBER(38,0),
+  MONTH NUMBER(38,0),
+  REVENUE FLOAT
+);
+
+COPY into MONTHLY_REVENUE
+  from @monthly_revenue_data_stage;
+
+CREATE or REPLACE TABLE BUDGET_ALLOCATIONS_AND_ROI (
+  MONTH varchar(30),
+  SEARCHENGINE integer,
+  SOCIALMEDIA integer,
+  VIDEO integer,
+  EMAIL integer,
+  ROI float
+);
+
+INSERT INTO BUDGET_ALLOCATIONS_AND_ROI (MONTH, SEARCHENGINE, SOCIALMEDIA, VIDEO, EMAIL, ROI)
+VALUES
+('January',35,50,35,85,8.22),
+('February',75,50,35,85,13.90),
+('March',15,50,35,15,7.34),
+('April',25,80,40,90,13.23),
+('May',95,95,10,95,6.246),
+('June',35,50,35,85,8.22);
+
+CREATE OR REPLACE STAGE Rachid_sprocs;
+CREATE OR REPLACE STAGE Rachid_models;
+CREATE OR REPLACE STAGE Rachid_udfs;
